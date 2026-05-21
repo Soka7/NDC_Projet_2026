815d20bd0c2af136717e081f1502aae255bf6329
Soka7 <kids.anguita@gmail.com>
2026-05-21 19:52:03 +0200

    msg

--- Lootbx.py
+++ Lootbx.py
@@ -1,6 +1,44 @@
 import pyxel
 from random import randint
 
+class Perso:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.Rotation = None
+        self.compteur = 0
+        
+    def Angle(self):
+        self.Rotation = degrees(atan2(self.y - pyxel.mouse_y, self.x - pyxel.mouse_x))
+    def Zone(self):
+        cx = self.x + 8
+        cy = self.y + 8
+        r=25
+        rect(0, 0, 256, cy-r, 0)
+        rect(0, cy+r, 256, 256, 0)
+        rect(0, 0, cx-r, 256, 0)
+        rect(0, cx+r, 256, 256, 0)
+    def Go(self):
+        liste = [0]
+        if btn(KEY_RIGHT) and pget(self.x + 16, self.y + 0) in liste:
+            self.x += 16
+            self.y += 0
+        if btn(KEY_UP) and pget(self.x + 16, self.y + 0) in liste:
+            self.x += 0
+            self.y -= 16
+        if btn(KEY_DOWN) and pget(self.x + 16, self.y + 0) in liste:
+            self.x += 0
+            self.y += 16
+        if btn(KEY_LEFT) and pget(self.x -16, self.y + 0) in liste:
+            self.x -= 16
+            self.y += 0
+    def Draw(self):
+        bltm(0, 0, 0, 0, 0, 256, 256, colkey=4, scale=1)
+        blt(self.x, self.y, 0, 96, 128, 16, 16, 4)
+        blt(self.x-14, self.y-4, 0, 24, 96, 32, 16, 4, rotate = self.Rotation)
+        
+Main = Perso(16, 16)
+
 class elements_obtenable:
     def __init__(self, nombre, boosts):
         self.nombre = []
